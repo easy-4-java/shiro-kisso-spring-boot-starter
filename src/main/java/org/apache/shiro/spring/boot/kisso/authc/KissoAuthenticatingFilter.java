51b5d81ae9d3b1e3dbf0b76a282cad5bb8c808fc
wandl <hiwepy@163.com>
2026-05-27 11:31:09 +0800

    chore(pom): align to Spring Boot 3.0.x (JDK 17, Boot 3.0.13)

--- src/main/java/org/apache/shiro/spring/boot/kisso/authc/KissoAuthenticatingFilter.java
+++ src/main/java/org/apache/shiro/spring/boot/kisso/authc/KissoAuthenticatingFilter.java
@@ -18,10 +18,10 @@ package org.apache.shiro.spring.boot.kisso.authc;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
