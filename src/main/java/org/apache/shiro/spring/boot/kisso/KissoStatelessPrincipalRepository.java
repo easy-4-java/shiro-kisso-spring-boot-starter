f26cd8e249a0bcfd439f95f80008b4983285b031
wandl-6A72h <hiwepy@gmail.com>
2026-07-31 22:22:52 +0800

    refactor: 包名 io.github.hiwepy 改为 io.github.easy4j

--- src/main/java/org/apache/shiro/spring/boot/kisso/KissoStatelessPrincipalRepository.java
+++ src/main/java/org/apache/shiro/spring/boot/kisso/KissoStatelessPrincipalRepository.java
@@ -27,9 +27,9 @@ import org.apache.shiro.biz.authz.principal.ShiroPrincipalRepositoryImpl;
 import org.apache.shiro.spring.boot.kisso.token.KissoAccessToken;
 
 import com.baomidou.kisso.security.token.SSOToken;
-import io.github.hiwepy.jwt.JwtClaims;
-import io.github.hiwepy.jwt.JwtPayload.RolePair;
-import io.github.hiwepy.jwt.utils.StringUtils;
+import io.github.easy4j.jwt.JwtClaims;
+import io.github.easy4j.jwt.JwtPayload.RolePair;
+import io.github.easy4j.jwt.utils.StringUtils;
 import com.google.common.collect.Sets;
 
 /**
